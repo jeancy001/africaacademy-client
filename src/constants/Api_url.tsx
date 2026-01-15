c3f886409b8df546186d76ca2cb94d18376dfa3e
jeancy001 <jeancympoy24@gmail.com>
2026-01-15 21:04:43 +0100

    added

--- src/constants/Api_url.tsx
+++ src/constants/Api_url.tsx
@@ -2,4 +2,4 @@ const isLocal = true; // or false
 
 export const API_URL = isLocal
   ? "http://localhost:5000/api/v1"
-  : "https://africaacademy-server.vercel.app/api/v1";
+  : "https://africaacademy-server.onrender.com/api/v1";
